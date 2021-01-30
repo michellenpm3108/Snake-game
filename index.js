91b495b671925ad24e2b777c7c884b68886b6375
Michelle <michelle.npm3108@gmail.com>
2021-01-31 05:48:46 +0800

    pop and unshift the snake

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const startButton = document.getElementById('start')
 const score = document.getElementById('score')
 const grid = document.querySelector('.grid')
 let squares = []
+let currentSnake = [2,1,0]
 
 function createGrid() {
-    for (let i=0 ; i< 100 ; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square')
-        grid.appendChild(square)
-        squares.push(square)
-    }
+ for (let i=0; i < 100 ; i++) {
+     const square = document.createElement('div')
+     square.classList.add('square')
+     grid.appendChild(square)
+     squares.push(square)
+ }
 }
-
 createGrid()
+currentSnake.forEach(index => squares[index]. classList.add('snake'))
+
+function move() {
+    let tail = currentSnake.pop()
+    squares[tail].classList.remove('snake')
+
+    currentSnake.unshift(currentSnake[0] + 1)
+    squares[currentSnake[0]].classList.add('snake')
+}
+move()
