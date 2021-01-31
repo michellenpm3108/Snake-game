82fceeb752f541a621db218340a2aa4806cf5080
Michelle <michelle.npm3108@gmail.com>
2021-02-01 07:46:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,29 @@ const score = document.getElementById('score')
 const grid = document.querySelector('.grid')
 let squares = []
 let currentSnake = [2,1,0]
+let direction = 1
 
 function createGrid() {
- for (let i=0; i < 100 ; i++) {
-     const square = document.createElement('div')
-     square.classList.add('square')
-     grid.appendChild(square)
-     squares.push(square)
- }
+    for(let i=0 ; i < 100 ; i++) {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        grid.appendChild(square)
+        squares.push(square)
+    }
 }
+
 createGrid()
-currentSnake.forEach(index => squares[index]. classList.add('snake'))
+
+currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move() {
     let tail = currentSnake.pop()
     squares[tail].classList.remove('snake')
-
-    currentSnake.unshift(currentSnake[0] + 1)
+    currentSnake.unshift(currentSnake[0]+direction)
     squares[currentSnake[0]].classList.add('snake')
 }
+
 move()
+
+setInterval(move, 1000)
+
